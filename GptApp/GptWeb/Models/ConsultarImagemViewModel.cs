37ebc27240b5a0b086b67e875585129790415023
cerqueirav <victorcerx@gmail.com>
2023-04-16 01:57:54 -0300

    Adiciona a solução web

---
+++ GptApp/GptWeb/Models/ConsultarImagemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace GptWeb.Models
+{
+    public class ConsultarImagemViewModel
+    {
+        [Display(Name = "Descrição")]
+        public string title { get; set; }
+
+        [Display(Name = "Quantidade")]
+        public int qty { get; set; }
+
+        [Display(Name = "Vincular ao Usuario")]
+        public bool save { get; set; }
+    }
+}
---
+++ GptApp/GptWeb/Models/DetalharImagem.cs
@@ -0,0 +1,11 @@
+﻿namespace GptWeb.Models
+{
+    public class DetalharImagem
+    {
+        public List<string> imagens { get; set; }
+
+        public DetalharImagem() {
+            imagens = new List<string>();
+        }
+    }
+}
